548d03687a8ff746d9a301326191f521c408cfea
GENYF <gnyf0915@gmail.com>
2021-01-12 15:52:12 +0900

    Update QuickAjou

---
+++ options.js
@@ -0,0 +1,72 @@
+const itemLink = ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "사이트맵"];
+
+function ItemLinkOptions(page, itemLink, num) {
+    for (let item of itemLink) {
+        let button = document.createElement('button');
+        button.textContent = item
+        button.addEventListener('click', function() {
+            switch(num) {
+                case 1: 
+                    chrome.storage.sync.set({Link1: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                case 2: 
+                    chrome.storage.sync.set({Link2: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                case 3: 
+                    chrome.storage.sync.set({Link3: item}, function() { 
+                        console.log(item); 
+                    });
+                     break;
+                case 4: 
+                    chrome.storage.sync.set({Link4: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                case 5: 
+                    chrome.storage.sync.set({Link5: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                case 6: 
+                    chrome.storage.sync.set({Link6: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                case 7: 
+                    chrome.storage.sync.set({Link7: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                case 8: 
+                    chrome.storage.sync.set({Link8: item}, function() { 
+                        console.log(item); }); 
+                    break;
+                case 9: 
+                    chrome.storage.sync.set({Link9: item}, function() { 
+                        console.log(item); 
+                    }); 
+                    break;
+                default :
+                    break;
+            }
+        });
+    page.appendChild(button);
+    }
+}
+
+window.onload = function(){
+    ItemLinkOptions(document.getElementById('button-link-1'), itemLink, 1);
+    ItemLinkOptions(document.getElementById('button-link-2'), itemLink, 2);
+    ItemLinkOptions(document.getElementById('button-link-3'), itemLink, 3);
+    ItemLinkOptions(document.getElementById('button-link-4'), itemLink, 4);
+    ItemLinkOptions(document.getElementById('button-link-5'), itemLink, 5);
+    ItemLinkOptions(document.getElementById('button-link-6'), itemLink, 6);
+    ItemLinkOptions(document.getElementById('button-link-7'), itemLink, 7);
+    ItemLinkOptions(document.getElementById('button-link-8'), itemLink, 8);
+    ItemLinkOptions(document.getElementById('button-link-9'), itemLink, 9);
+}
+
--- popup.js
+++ popup.js
@@ -1,5 +1,51 @@
+//바로가기 객체, 바로가기 수에 따라 변동
+var homepage = {
+    URL : "https://www.ajou.ac.kr/kr/index.do",
+    IMG : "images/icon/Ajou_homepage.png"
+}
+var portal = {
+    URL : "https://mportal.ajou.ac.kr/main.do",
+    IMG : "images/icon/Ajou_portal.png"
+}
+var univService = {
+    URL : "https://mhaksa.ajou.ac.kr:30443",
+    IMG : "images/icon/Ajou_univService.png"
+}
+var blackborad = {
+    URL : "https://eclass2.ajou.ac.kr",
+    IMG : "images/icon/Ajou_blackboard.png"
+}
+var checkBook = {
+    URL : "https://attend.ajou.ac.kr/eams/student/main",
+    IMG : "images/icon/Ajou_checkBook.png"
+}
+var calendar = {
+    URL : "https://www.ajou.ac.kr/kr/bachelor/schedule.do",
+    IMG : "images/icon/Ajou_calendar.png"
+}
+var library = {
+    URL : "https://www.ajou.ac.kr/kr/admission/list01.do",
+    IMG : "images/icon/Ajou_library.png"
+}
+var department = {
+    URL : "https://www.ajou.ac.kr/kr/admission/list01.do",
+    IMG : "images/icon/Ajou_department.png"
+}
+var sitemap = {
+    URL : "https://www.ajou.ac.kr/kr/guide/sitemap.do",
+    IMG : "images/icon/Ajou_sitemap.png"
+}
+
 document.addEventListener('DOMContentLoaded', function(){
-    //마우스 우클릭 및 드래그 방지
+    mouseSecurity();
+    enterSearch();
+    buttonSearch();
+    changeLink();
+    printDate();
+});
+
+//마우스 우클릭 및 드래그 방지 함수
+function mouseSecurity(){
     var main = document.getElementById("main");
     main.addEventListener("contextmenu", function(event){
         event.preventDefault();
@@ -7,17 +53,10 @@ document.addEventListener('DOMContentLoaded', function(){
     main.addEventListener("dragstart", function(event){
         event.preventDefault();
     });
+}
 
-    //날짜 표시
-    let today = new Date();   
-    let year = today.getFullYear();
-    let month = today.getMonth() + 1;
-    let date = today.getDate();
-
-    var dayText = year + "/" + month + "/" + date;
-    document.getElementById("day").innerHTML = dayText;
-
-    //엔터로 검색
+//엔터 검색 함수
+function enterSearch(){
     var input = document.getElementById("searchInput");
     input.addEventListener("keypress", function(event) {
         if (event.keyCode == 13) {
@@ -26,17 +65,115 @@ document.addEventListener('DOMContentLoaded', function(){
             search(text);
         }
     });
+}
 
-    //버튼으로 검색
+//버튼 검색 함수
+function buttonSearch(){
     var button = document.getElementById("searchButton");   
     button.addEventListener("click", function(){
         var text = document.getElementById("searchInput").value;
         search(text);
     });
-});
+}
 
 //검색 함수
 function search(text){
     var link = "https://www.ajou.ac.kr/kr/search.do?qt=" + text;
     window.open(link);
+}
+
+//바로가기 변경 함수
+function changeLink(){
+    chrome.storage.sync.get('Link1', function(data) {
+        connectionLink(document.getElementById('item-link-1'), data.Link1);
+    });
+    chrome.storage.sync.get('Link2', function(data) {
+        connectionLink(document.getElementById('item-link-2'), data.Link2);
+    });
+    chrome.storage.sync.get('Link3', function(data) {
+        connectionLink(document.getElementById('item-link-3'), data.Link3)
+    });
+    chrome.storage.sync.get('Link4', function(data) {
+        connectionLink(document.getElementById('item-link-4'), data.Link4);
+    });
+    chrome.storage.sync.get('Link5', function(data) {
+        connectionLink(document.getElementById('item-link-5'), data.Link5);
+    });
+    chrome.storage.sync.get('Link6', function(data) {
+        connectionLink(document.getElementById('item-link-6'), data.Link6)
+    });
+    chrome.storage.sync.get('Link7', function(data) {
+        connectionLink(document.getElementById('item-link-7'), data.Link7);
+    });
+    chrome.storage.sync.get('Link8', function(data) {
+        connectionLink(document.getElementById('item-link-8'), data.Link8);
+    });
+    chrome.storage.sync.get('Link9', function(data) {
+        connectionLink(document.getElementById('item-link-9'), data.Link9)
+    });
+}
+
+//바로가기 객체 연결 함수, 바로가기 수에 따라 변동
+function connectionLink(itemLink, Link){
+    switch(Link){
+        case "홈페이지": 
+            var URL = homepage.URL; 
+            var IMG = homepage.IMG; 
+            break;
+        case "포탈": 
+            var URL = portal.URL; 
+            var IMG = portal.IMG; 
+            break;
+        case "학사서비스": 
+            var URL = univService.URL; 
+            var IMG = univService.IMG; 
+            break;
+        case "아주Bb": 
+            var URL = blackborad.URL; 
+            var IMG = blackborad.IMG; 
+            break;
+        case "전자출석부": 
+            var URL = checkBook.URL; 
+            var IMG = checkBook.IMG; 
+            break;
+        case "학사일정":
+            var URL = calendar.URL; 
+            var IMG = calendar.IMG; 
+            break;
+        case "중앙도서관":
+            var URL = library.URL; 
+            var IMG = library.IMG; 
+            break;
+        case "학과 홈페이지":
+            var URL = department.URL; 
+            var IMG = department.IMG; 
+            break;
+        case "사이트맵":
+            var URL = sitemap.URL; 
+            var IMG = sitemap.IMG; 
+            break;
+        default:
+            break;
+    }
+
+    itemLink.innerHTML = '<a href=' + URL + ' target="_blank"><img src='+ IMG + '><p>' + Link + '</p></a>';
+}
+
+//날짜 출력 함수
+function printDate(){
+    let today = new Date();   
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let date = today.getDate();
+
+    if (month < 10){
+        month = "0" + month;
+    }
+
+    if (date < 10){
+        date = "0" + date;
+    }
+
+    var dayText = year + "/" + month + "/" + date;
+    document.getElementById("day").innerHTML = dayText;
 }
