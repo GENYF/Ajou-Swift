e4f42ac8bfd97d0e0c1ea22d3cfd7cc64d20cf77
GENYF <gnyf0915@gmail.com>
2021-01-13 20:51:01 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({Color : "white", Link1: "홈페이지", Link2: "포탈", Link3: "학사서비스", Link4: "아주Bb", Link5: "전자출석부", Link6: "학사일정", Link7: "중앙도서관", Link8: "학과 홈페이지", Link9: "사이트맵"}, function() {
+    chrome.storage.sync.set({Color : "white", Link1: "홈페이지", Link2: "포탈", Link3: "학사서비스", Link4: "아주Bb", Link5: "전자출석부", Link6: "학사일정", Link7: "요람", Link8: "중앙도서관", Link9: "사이트맵"}, function() {
         console.log();
     });
 });
