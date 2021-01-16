f07adf8366e8a4439d0ae096bb798c1694760433
GENYF <gnyf0915@gmail.com>
2021-01-16 19:02:52 +0900

    Update options.js

--- options.js
+++ options.js
@@ -1,6 +1,14 @@
 const itemColor = ["white", "black"];
 const itemLink = ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "사이트맵", "공지사항", "수강신청", "요람", "대학기구", "교내 전화번호", "대학일자리센터", "LINC+ 사업단", "수학 웹과제", "과학 웹과제"];
 
+window.onload = function(){
+    ItemColorOptions(document.getElementById('button-color'), itemColor);
+
+    for (let i = 1; i <= 9; i++){
+        ItemLinkOptions(document.getElementById(`button-link-${i}`), itemLink, i);
+    }
+}
+
 function ItemColorOptions(page, itemColor) {
     for (let item of itemColor) {
         let button = document.createElement('button');
@@ -27,11 +35,4 @@ function ItemLinkOptions(page, itemLink, num) {
     }
 }
 
-window.onload = function(){
-    ItemColorOptions(document.getElementById('button-color'), itemColor);
-
-    for (let i = 1; i <= 9; i++){
-        ItemLinkOptions(document.getElementById(`button-link-${i}`), itemLink, i);
-    }
-}
 
