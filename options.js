58d41e78202291199b394f643b716f356760614b
GENYF <gnyf0915@gmail.com>
2021-01-16 15:26:51 +0900

    Update QuickAjou

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 const itemColor = ["white", "black"];
-const itemLink = ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "사이트맵", "공지사항", "수강신청", "요람"];
+const itemLink = ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "사이트맵", "공지사항", "수강신청", "요람", "대학기구", "교내 전화번호", "대학일자리센터", "LINC+ 사업단", "수학 웹과제", "과학 웹과제"];
 
 function ItemColorOptions(page, itemColor) {
     for (let item of itemColor) {
@@ -19,54 +19,9 @@ function ItemLinkOptions(page, itemLink, num) {
         let button = document.createElement('button');
         button.textContent = item
         button.addEventListener('click', function() {
-            switch(num) {
-                case 1: 
-                    chrome.storage.sync.set({Link1: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                case 2: 
-                    chrome.storage.sync.set({Link2: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                case 3: 
-                    chrome.storage.sync.set({Link3: item}, function() { 
-                        console.log(item); 
-                    });
-                     break;
-                case 4: 
-                    chrome.storage.sync.set({Link4: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                case 5: 
-                    chrome.storage.sync.set({Link5: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                case 6: 
-                    chrome.storage.sync.set({Link6: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                case 7: 
-                    chrome.storage.sync.set({Link7: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                case 8: 
-                    chrome.storage.sync.set({Link8: item}, function() { 
-                        console.log(item); }); 
-                    break;
-                case 9: 
-                    chrome.storage.sync.set({Link9: item}, function() { 
-                        console.log(item); 
-                    }); 
-                    break;
-                default :
-                    break;
-            }
+            chrome.storage.sync.set({[`Link${num}`] : item}, function() { 
+                console.log(item); 
+            }); 
         });
     page.appendChild(button);
     }
--- popup.js
+++ popup.js
@@ -1,53 +1,3 @@
-//바로가기 객체, 바로가기 수에 따라 변동
-let homepage = {
-    URL : "https://www.ajou.ac.kr/kr/index.do",
-    IMG : "images/icon/Ajou_homepage.png"
-}
-let portal = {
-    URL : "https://mportal.ajou.ac.kr/main.do",
-    IMG : "images/icon/Ajou_portal.png"
-}
-let univService = {
-    URL : "https://mhaksa.ajou.ac.kr:30443",
-    IMG : "images/icon/Ajou_univService.png"
-}
-let blackborad = {
-    URL : "https://eclass2.ajou.ac.kr",
-    IMG : "images/icon/Ajou_blackboard.png"
-}
-let checkBook = {
-    URL : "https://attend.ajou.ac.kr/eams/student/main",
-    IMG : "images/icon/Ajou_checkBook.png"
-}
-let calendar = {
-    URL : "https://www.ajou.ac.kr/kr/bachelor/schedule.do",
-    IMG : "images/icon/Ajou_calendar.png"
-}
-let library = {
-    URL : "https://library.ajou.ac.kr",
-    IMG : "images/icon/Ajou_library.png"
-}
-let department = {
-    URL : "https://www.ajou.ac.kr/kr/admission/list01.do",
-    IMG : "images/icon/Ajou_department.png"
-}
-let sitemap = {
-    URL : "https://www.ajou.ac.kr/kr/guide/sitemap.do",
-    IMG : "images/icon/Ajou_sitemap.png"
-}
-let notice = {
-    URL : "https://www.ajou.ac.kr/kr/ajou/notice.do",
-    IMG : "images/icon/Ajou_notice.png"
-}
-let applyClass = {
-    URL : "http://sugang.ajou.ac.kr/",
-    IMG : "images/icon/Ajou_applyClass.png"
-}
-let curriculum = {
-    URL : "https://www.ajou.ac.kr/kr/bachelor/handbook2019.do",
-    IMG : "images/icon/Ajou_curriculum.png"
-}
-
 //메인 함수, DOM 로드되면 실행
 document.addEventListener('DOMContentLoaded', function(){
     mouseSecurity();
@@ -55,26 +5,26 @@ document.addEventListener('DOMContentLoaded', function(){
     buttonSearch();
     changeColor();
     changeLink();
-    printDate();
+    printSchedule();
 });
 
 //마우스 우클릭 및 드래그 방지 함수
 function mouseSecurity(){
-    let main = document.getElementById("main");
-    main.addEventListener("contextmenu", function(event){
+    let main = document.getElementById('main');
+    main.addEventListener('contextmenu', function(event){
         event.preventDefault();
     });
-    main.addEventListener("dragstart", function(event){
+    main.addEventListener('dragstart', function(event){
         event.preventDefault();
     });
 }
 
 //엔터 검색 함수
 function enterSearch(){
-    let input = document.getElementById("searchInput");
-    input.addEventListener("keypress", function(event) {
+    let input = document.getElementById('searchInput');
+    input.addEventListener('keypress', function(event) {
         if (event.keyCode == 13) {
-            let text = document.getElementById("searchInput").value;
+            let text = document.getElementById('searchInput').value;
             console.log(text);
             search(text);
         }
@@ -83,9 +33,9 @@ function enterSearch(){
 
 //버튼 검색 함수
 function buttonSearch(){
-    let button = document.getElementById("searchButton");   
-    button.addEventListener("click", function(){
-        let text = document.getElementById("searchInput").value;
+    let button = document.getElementById('searchButton');   
+    button.addEventListener('click', function(){
+        let text = document.getElementById('searchInput').value;
         search(text);
     });
 }
@@ -99,8 +49,7 @@ function search(text){
 //다크모드 설정 함수
 function changeColor(){
     chrome.storage.sync.get('Color', function(data) {
-        console.log(data.Color);
-        let main = document.getElementById("main");
+        let main = document.getElementById('main');
 
         if(data.Color == "black"){
             main.style.backgroundColor = "#35363A";
@@ -109,75 +58,26 @@ function changeColor(){
     });
 }
 
-//바로가기 변경 함수
+//바로가기 생성 및 변경 함수
 function changeLink() {
     for (let i = 1; i <= 9; i++) {
       chrome.storage.sync.get(`Link${i}`, function(data) {
-        connectLink(document.getElementById(`item-link-${i}`), data[`Link${i}`]);
+        let itemLink = document.getElementById(`item-link-${i}`);
+        let Link = data[`Link${i}`];
+
+        fetch(chrome.extension.getURL('page.json'))
+        .then((resp) => resp.json())
+        .then(function (jsonData) {
+            let URL = jsonData[`${Link}`].URL; 
+            let IMG = jsonData[`${Link}`].IMG;
+            itemLink.innerHTML = '<a href=' + URL + ' target="_blank"><img src='+ IMG + '><p>' + Link + '</p></a>'; 
+        });
       });
     }
 }
 
-//바로가기 객체 연결 함수, 바로가기 수에 따라 변동
-function connectLink(itemLink, Link){
-    switch(Link){
-        case "홈페이지": 
-            var URL = homepage.URL; 
-            var IMG = homepage.IMG; 
-            break;
-        case "포탈": 
-            var URL = portal.URL; 
-            var IMG = portal.IMG; 
-            break;
-        case "학사서비스": 
-            var URL = univService.URL; 
-            var IMG = univService.IMG; 
-            break;
-        case "아주Bb": 
-            var URL = blackborad.URL; 
-            var IMG = blackborad.IMG; 
-            break;
-        case "전자출석부": 
-            var URL = checkBook.URL; 
-            var IMG = checkBook.IMG; 
-            break;
-        case "학사일정":
-            var URL = calendar.URL; 
-            var IMG = calendar.IMG; 
-            break;
-        case "중앙도서관":
-            var URL = library.URL; 
-            var IMG = library.IMG; 
-            break;
-        case "학과 홈페이지":
-            var URL = department.URL; 
-            var IMG = department.IMG; 
-            break;
-        case "사이트맵":
-            var URL = sitemap.URL; 
-            var IMG = sitemap.IMG; 
-            break;
-        case "공지사항":
-            var URL = notice.URL; 
-            var IMG = notice.IMG; 
-            break;
-        case "수강신청":
-            var URL = applyClass.URL; 
-            var IMG = applyClass.IMG; 
-            break;
-        case "요람":
-            var URL = curriculum.URL; 
-            var IMG = curriculum.IMG; 
-            break;
-        default:
-            break;
-    }
-
-    itemLink.innerHTML = '<a href=' + URL + ' target="_blank"><img src='+ IMG + '><p>' + Link + '</p></a>';
-}
-
-//날짜 출력 함수
-function printDate(){
+//날짜 및 일정 출력 함수
+function printSchedule(){
     let today = new Date();   
     let year = today.getFullYear();
     let month = today.getMonth() + 1;
@@ -192,5 +92,17 @@ function printDate(){
     }
 
     let dayText = year + "-" + month + "-" + date;
-    document.getElementById("day").innerHTML = dayText;
+    document.getElementById('day').innerHTML = dayText;
+
+    fetch(chrome.extension.getURL('schedule.json'))
+    .then((resp) => resp.json())
+    .then(function (jsonData) {
+        let scheduleText = jsonData[`${dayText}`];
+        if(scheduleText == undefined){
+            document.getElementById('schedule').innerHTML = "학사일정이 없습니다.";
+        }
+        else{
+            document.getElementById('schedule').innerHTML = scheduleText;
+        }
+    });
 }
