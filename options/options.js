c67c707ab16aa3d4ffbf39a574138ffc7c73a760
GENYF <gnyf0915@gmail.com>
2021-02-19 20:32:06 +0900

    Update Options
    Add department select HTML

--- options/options.js
+++ options/options.js
@@ -1,5 +1,6 @@
 const itemColor = ["white", "black"];
 const itemLink = ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "사이트맵", "공지사항", "수강신청", "요람", "대학기구", "교내 전화번호", "대학일자리센터", "LINC+ 사업단", "수학 웹과제", "과학 웹과제"];
+const itemDepartment = ["소프트웨어학과"]
 
 window.onload = function(){
     ItemColorOptions(document.getElementById('button-color'), itemColor);
