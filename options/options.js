1bf022b642372ab1a385b9478c3218f2ff28cd82
GENYF <gnyf0915@gmail.com>
2021-02-23 22:19:54 +0900

    Update Ajou-Swift
    Delete innerHTML and P tag
    Add code comment

--- options/options.js
+++ options/options.js
@@ -1,6 +1,6 @@
 const itemColor = ["white", "black"];
 const itemLink =  ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "나의 학과", "사이트맵", "공지사항", "수강신청", "요람", "대학기구", "교내 전화번호", "대학일자리센터", "LINC+ 사업단", "수학 웹과제", "과학 웹과제"];
-const itemDepartment = ["소프트웨어학과", "국방디지털융합과", "미디어학과"];
+const itemDepartment = ["전자공학과", "소프트웨어학과", "사이버보안학과", "국방디지털융합학과", "미디어학과"];
 
 /* json 데이터에서 itemList 파싱, 구현 예정
 fetch(chrome.extension.getURL('../data/page.json'))
@@ -19,10 +19,13 @@ window.addEventListener('load', function(){
     }
 });
 
+//테마 선택 옵션 생성
 function itemColorOptions(page, itemColor) {
     for (let item of itemColor) {
         let button = document.createElement('button');
         button.textContent = item;
+
+        //버튼 클릭시 테마 설정 동기화
         button.addEventListener('click', function() {
             chrome.storage.sync.set({Color: item}, function() { 
                 console.log(item); 
@@ -33,6 +36,7 @@ function itemColorOptions(page, itemColor) {
     }
 }
 
+//학과 선택 옵션 생성
 function itemDepartmentOptions(formPage, selectPage, itemDepartment){
     for (let item of itemDepartment) {
         var select = document.createElement('option');
@@ -45,6 +49,8 @@ function itemDepartmentOptions(formPage, selectPage, itemDepartment){
     let button = document.createElement('button');
     button.id = "button-department";
     button.textContent = "제출";
+
+    //버튼 클릭시 학과 설정 동기화
     button.addEventListener('click', function() {
         let data = selectPage.options[selectPage.selectedIndex].value;
         
@@ -56,10 +62,13 @@ function itemDepartmentOptions(formPage, selectPage, itemDepartment){
     formPage.appendChild(button);
 }
 
+//바로가기 선택 옵션 생성
 function itemLinkOptions(page, itemLink, num) {
     for (let item of itemLink) {
         let button = document.createElement('button');
         button.textContent = item
+
+        //버튼 클릭시 바로가기 설정 동기화
         button.addEventListener('click', function() {
             chrome.storage.sync.set({[`Link${num}`] : item}, function() { 
                 console.log(item); 
--- popup/popup.js
+++ popup/popup.js
@@ -12,9 +12,11 @@ window.addEventListener('DOMContentLoaded', function(){
 //마우스 우클릭 및 드래그 방지 함수
 function mouseSecurity(){
     let main = document.getElementById('main');
+
     main.addEventListener('contextmenu', function(event){
         event.preventDefault();
     });
+
     main.addEventListener('dragstart', function(event){
         event.preventDefault();
     });
@@ -23,11 +25,12 @@ function mouseSecurity(){
 //엔터 검색 함수
 function enterSearch(){
     let input = document.getElementById('searchInput');
+
     input.addEventListener('keypress', function(event) {
         if (event.keyCode == 13) {
-            let text = document.getElementById('searchInput').value;
-            console.log(text);
-            search(text);
+            let query = document.getElementById('searchInput').value;
+            console.log(query);
+            search(query);
         }
     });
 }
@@ -35,67 +38,94 @@ function enterSearch(){
 //버튼 검색 함수
 function buttonSearch(){
     let button = document.getElementById('searchButton');   
+
     button.addEventListener('click', function(){
-        let text = document.getElementById('searchInput').value;
-        search(text);
+        let query = document.getElementById('searchInput').value;
+        search(query);
     });
 }
 
 //검색 함수
-function search(text){
-    let link = "https://www.ajou.ac.kr/kr/search.do?qt=" + text;
+function search(query){
+    let link = "https://www.ajou.ac.kr/kr/search.do?qt=" + query;
     window.open(link);
 }
 
 //다크모드 설정 함수
 function changeColor(){
+    //크롬 스토리지에 동기화된 사용자 바로가기 데이터 가져오기
     chrome.storage.sync.get('Color', function(data) {
-        let main = document.getElementById('main');
-
+        
+        //다크모드 설정
         if(data.Color == "black"){
+            let main = document.getElementById('main');
+
             main.style.backgroundColor = "#35363A";
             main.style.filter = "invert(100%) contrast(80%) saturate(120%) brightness(120%)";
         }
     });
 }
 
-//바로가기 생성 및 변경 함수, innerHTML 보안상 수정 필요
+//바로가기 생성 및 변경 함수
 function changeLink() {
+    //9개의 바로가기 생성
     for (let i = 1; i <= 9; i++) {
-      chrome.storage.sync.get(`Link${i}`, function(data) {
-        let itemLink = document.getElementById(`item-link-${i}`);
-        let Link = data[`Link${i}`];
+        //크롬 스토리지에 동기화된 사용자 바로가기 데이터 가져오기
+        chrome.storage.sync.get(`Link${i}`, function(data) {
+            let itemLink = document.getElementById(`item-link-${i}`);
+            let Link = data[`Link${i}`];
 
-        fetch(chrome.extension.getURL("../data/page.json"))
-        .then((response) => response.json())
-        .then(function (jsonData) {
-            let URL = jsonData[`${Link}`].URL; 
-            let IMG = jsonData[`${Link}`].IMG;
+            //json에서 Ajax로 페이지 이미지와 링크 불러오기
+            fetch(chrome.extension.getURL("../data/page.json"))
+            .then((response) => response.json())
+            .then(function (jsonData) {
+                let URL = jsonData[`${Link}`].URL; 
+                let IMG = jsonData[`${Link}`].IMG;
 
-            itemLink.innerHTML = '<a href=' + URL + ' target="_blank"><img src='+ IMG + '><p>' + Link + '</p></a>'; 
-            
-            //a 태그 삭제 및 addEventListener 구현 필요
-            /*
-            let aTag = document.createElement('a');
-            aTag.setAttribute('herf', URL);
-            aTag.setAttribute('target', '_blank');
-            itemLink.appendChild(aTag);
+                //페이지 클릭 이벤트 추가 
+                itemLink.addEventListener('click',function(){
+                    window.open(URL);
+                });
+                itemLink.style.cursor = "pointer";
 
-            let imgTag = document.createElement('img');
-            imgTag.setAttribute('src', IMG);
-            aTag.appendChild(imgTag);
+                //이미지 생성
+                let imgTag = document.createElement('img');
+                imgTag.setAttribute('src', IMG);
+                itemLink.appendChild(imgTag);
 
-            let pTag = document.createElement('p');
-            pTag.textContent = Link;
-            aTag.appendChild(pTag);
-            */
+                //텍스트 생성
+                let pTag = document.createElement('p');
+                pTag.textContent = Link;
+                itemLink.appendChild(pTag);
+            });
         });
-      });
     }
 }
 
 //날짜 및 일정 출력 함수
 function printSchedule(){
+    //페이지 클릭 이벤트 추가
+    let itemSchedule = document.getElementById('item-schedule');
+
+    itemSchedule.addEventListener('click',function(){
+        window.open("https://www.ajou.ac.kr/kr/bachelor/schedule.do");
+    });
+    itemSchedule.style.cursor = "pointer";
+
+    //json에서 Ajax로 학사일정 불러오기
+    fetch(chrome.extension.getURL("../data/schedule.json"))
+    .then((response) => response.json())
+    .then(function (jsonData) {
+        let scheduleText = jsonData[`${dayText}`];
+        if(scheduleText == undefined){
+            schedule.textContent = "학사일정이 없습니다.";
+        }
+        else{
+            schedule.textContent = scheduleText;
+        }
+    });
+
+    //날짜 불러오기
     let today = new Date();   
     let year = today.getFullYear();
     let month = today.getMonth() + 1;
@@ -112,15 +142,5 @@ function printSchedule(){
     let dayText = year + "-" + month + "-" + date;
     document.getElementById('day').textContent = dayText;
 
-    fetch(chrome.extension.getURL("../data/schedule.json"))
-    .then((response) => response.json())
-    .then(function (jsonData) {
-        let scheduleText = jsonData[`${dayText}`];
-        if(scheduleText == undefined){
-            document.getElementById('schedule').textContent = "학사일정이 없습니다.";
-        }
-        else{
-            document.getElementById('schedule').textContent = scheduleText;
-        }
-    });
+    let schedule = document.getElementById('schedule');
 }
