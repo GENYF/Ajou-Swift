b153c8139abd4a687ba518625bca7b0fb90a79b7
GENYF <gnyf0915@gmail.com>
2021-02-24 19:33:08 +0900

    Update Ajou-Swift
    Clean up code

--- options/options.js
+++ options/options.js
@@ -1,8 +1,9 @@
-const itemColor = ["white", "black"];
-const itemLink =  ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "나의 학과", "사이트맵", "공지사항", "수강신청", "요람", "기숙사", "교내식당", "대학기구", "교내 전화번호", "대학일자리센터", "LINC+ 사업단", "수학 웹과제", "과학 웹과제", "에브리타임"];
-const itemDepartment = ["전자공학과", "소프트웨어학과", "사이버보안학과", "국방디지털융합학과", "미디어학과"];
+const itemColorList = ["white", "black"];
+const itemLinkList =  ["홈페이지", "포탈", "학사서비스", "아주Bb", "전자출석부", "학사일정", "중앙도서관", "학과 홈페이지", "나의 학과", "사이트맵", "공지사항", "수강신청", "요람", "기숙사", "교내식당", "대학기구", "교내 전화번호", "대학일자리센터", "LINC+ 사업단", "수학 웹과제", "과학 웹과제", "에브리타임"];
+const itemDepartmentList = ["전자공학과", "소프트웨어학과", "사이버보안학과", "국방디지털융합학과", "미디어학과"];
 
-/* json 데이터에서 itemList 파싱, 구현 예정
+//json 데이터에서 LinkList 파싱, 구현 예정
+/* 
 fetch(chrome.extension.getURL('../data/page.json'))
 .then((resp) => resp.json())
 .then(function (jsonData) {
@@ -10,18 +11,15 @@ fetch(chrome.extension.getURL('../data/page.json'))
 });
 */
 
+//윈도우가 로드되면 실행
 window.addEventListener('load', function(){
-    itemColorOptions(document.getElementById('button-color'), itemColor);
-    itemDepartmentOptions(document.getElementById('form-department'), document.getElementById('select-department'), itemDepartment);
+    
+    /*테마 선택 옵션 생성 기능*/
 
-    for (let i = 1; i <= 9; i++){
-        itemLinkOptions(document.getElementById(`button-link-${i}`), itemLink, i);
-    }
-});
+    let buttonColorElement = document.getElementById('button-color')
 
-//테마 선택 옵션 생성
-function itemColorOptions(element, itemColor) {
-    for (let item of itemColor) {
+    //테마 선택 버튼 생성
+    for (let item of itemColorList) {
         let button = document.createElement('button');
         button.textContent = item;
 
@@ -32,13 +30,16 @@ function itemColorOptions(element, itemColor) {
             }); 
         });
 
-        element.appendChild(button);
+        buttonColorElement.appendChild(button);
     }
-}
 
-//학과 선택 옵션 생성
-function itemDepartmentOptions(formElement, selectElement, itemDepartment){
-    for (let item of itemDepartment) {
+    /*학과 선택 옵션 생성 기능*/
+
+    let formElement = document.getElementById('form-department');
+    let selectElement = document.getElementById('select-department')
+
+    //학과 선택 버튼 생성
+    for (let item of itemDepartmentList) {
         var select = document.createElement('option');
         select.setAttribute('value', item);
         select.textContent = item;
@@ -47,6 +48,7 @@ function itemDepartmentOptions(formElement, selectElement, itemDepartment){
     }
 
     let button = document.createElement('button');
+
     button.id = "button-department";
     button.textContent = "제출";
 
@@ -60,11 +62,17 @@ function itemDepartmentOptions(formElement, selectElement, itemDepartment){
     });
 
     formElement.appendChild(button);
-}
 
-//바로가기 선택 옵션 생성
-function itemLinkOptions(element, itemLink, num) {
-    for (let item of itemLink) {
+    /*바로가기 선택 옵션 생성 기능*/
+
+    for (let i = 1; i <= 9; i++){
+        itemLinkOptions(document.getElementById(`button-link-${i}`), itemLinkList, i);
+    }
+});
+
+//바로가기 선택 옵션 생성 함수
+function itemLinkOptions(buttonLinkElement, itemLinkList, num) {
+    for (let item of itemLinkList) {
         let button = document.createElement('button');
         button.textContent = item
 
@@ -75,8 +83,6 @@ function itemLinkOptions(element, itemLink, num) {
             }); 
         });
 
-        element.appendChild(button);
+        buttonLinkElement.appendChild(button);
     }
 }
-
-
--- popup/popup.js
+++ popup/popup.js
@@ -1,59 +1,8 @@
 //메인 함수, DOM 로드되면 실행
 window.addEventListener('DOMContentLoaded', function(){
-    mouseSecurity();
-    enterSearch();
-    buttonSearch();
-    changeColor();
-    setLink();
-    setSchedule();
-});
+    /*테마 변경 기능*/
 
-
-//마우스 우클릭 및 드래그 방지 함수
-function mouseSecurity(){
-    let main = document.getElementById('main');
-
-    main.addEventListener('dragstart', function(event){
-        event.preventDefault();
-    });
-
-    main.addEventListener('contextmenu', function(event){
-        event.preventDefault();
-    });
-}
-
-//엔터 검색 함수
-function enterSearch(){
-    let input = document.getElementById('searchInput');
-
-    input.addEventListener('keypress', function(event) {
-        if (event.keyCode == 13) {
-            let query = document.getElementById('searchInput').value;
-            console.log(query);
-            search(query);
-        }
-    });
-}
-
-//버튼 검색 함수
-function buttonSearch(){
-    let button = document.getElementById('searchButton');   
-
-    button.addEventListener('click', function(){
-        let query = document.getElementById('searchInput').value;
-        search(query);
-    });
-}
-
-//검색 함수
-function search(query){
-    let queryLink = "https://www.ajou.ac.kr/kr/search.do?qt=" + query;
-    window.open(queryLink);
-}
-
-//다크모드 설정 함수
-function changeColor(){
-    //크롬 스토리지에 동기화된 사용자 바로가기 데이터 가져오기
+    //크롬 스토리지에 동기화된 사용자 데이터 가져오기
     chrome.storage.sync.get('Color', function(data) {
         
         //다크모드 설정
@@ -64,65 +13,17 @@ function changeColor(){
             main.style.filter = "invert(100%) contrast(80%) saturate(120%) brightness(120%)";
         }
     });
-}
 
-//바로가기 생성 및 변경 함수
-function setLink() {
-    //9개의 바로가기 생성
+    /*바로가기 생성 및 변경 기능*/
+    
+    //바로가기 9개 생성 및 변경
     for (let i = 1; i <= 9; i++) {
-        //크롬 스토리지에 동기화된 사용자 바로가기 데이터 가져오기
-        chrome.storage.sync.get(`Link${i}`, function(data) {
-            //json에서 Ajax로 페이지 이미지와 링크 불러오기
-            let itemLink = document.getElementById(`item-link-${i}`);
-            let linkN = data[`Link${i}`];
-
-            fetch(chrome.extension.getURL("../data/page.json"))
-            .then((response) => response.json())
-            .then(function (jsonData) {
-                let URL = jsonData[`${linkN}`].URL; 
-                let IMG = jsonData[`${linkN}`].IMG;
-
-                //페이지 클릭 이벤트 추가 
-                clickOpenNewTeb(itemLink, URL);
-
-                //이미지 및 텍스트 생성
-                let linkImage = document.createElement('img');
-                let linkText = document.createElement('p');
-
-                linkImage.setAttribute('src', IMG);
-                linkText.textContent = linkN;
-
-                itemLink.appendChild(linkImage);
-                itemLink.appendChild(linkText);
-            });
-        });
+        setLink(document.getElementById(`item-link-${i}`), i);
     }
-}
 
-//날짜 및 일정 출력 함수
-function setSchedule(){
-    //페이지 클릭 이벤트 추가
-    let itemSchedule = document.getElementById('item-schedule');
+    /*날짜 및 학사일정 출력 기능*/
 
-    clickOpenNewTeb(itemSchedule, "https://www.ajou.ac.kr/kr/bachelor/schedule.do");
-
-    //json에서 Ajax로 학사일정 불러오기
-    let schedule = document.getElementById('schedule');
-
-    fetch(chrome.extension.getURL("../data/schedule.json"))
-    .then((response) => response.json())
-    .then(function (jsonData) {
-        let scheduleText = jsonData[`${dayText}`];
-
-        if(scheduleText == undefined){
-            schedule.textContent = "학사일정이 없습니다.";
-        }
-        else{
-            schedule.textContent = scheduleText;
-        }
-    });
-
-    //날짜 불러오기
+    //날짜 가져오기
     let today = new Date();   
     let year = today.getFullYear();
     let month = today.getMonth() + 1;
@@ -136,9 +37,97 @@ function setSchedule(){
         date = "0" + date;
     }
 
+    //날짜 출력
     let dayText = year + "-" + month + "-" + date;
     document.getElementById('day').textContent = dayText;
+
+    //학사일정 출력 
+    let schedule = document.getElementById('schedule');
+
+    //json에서 Ajax로 학사일정 불러오기
+    fetch(chrome.extension.getURL("../data/schedule.json"))
+    .then((response) => response.json())
+    .then(function (jsonData) {
+        let scheduleText = jsonData[`${dayText}`];
+
+        if(scheduleText == undefined){
+            schedule.textContent = "학사일정이 없습니다.";
+        }
+        else{
+            schedule.textContent = scheduleText;
+        }
+    });
+
+    //학사일정 페이지 바로가기 추가
+    let itemSchedule = document.getElementById('item-schedule');
+
+    clickOpenNewTeb(itemSchedule, "https://www.ajou.ac.kr/kr/bachelor/schedule.do");
+
+    /*엔터 및 버튼 검색 기능*/
+
+    //엔터 검색
+    let input = document.getElementById('searchInput');
+
+    input.addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+            let query = document.getElementById('searchInput').value;
+            console.log(query);
+            search(query);
+        }
+    });
+
+    //버튼 검색
+    let button = document.getElementById('searchButton');   
+
+    button.addEventListener('click', function(){
+        let query = document.getElementById('searchInput').value;
+        search(query);
+    });
+
+    /*마우스 우클릭 및 드래그 방지 기능*/
+
+    let main = document.getElementById('main');
+
+    //드래그 방지
+    main.addEventListener('dragstart', function(event){
+        event.preventDefault();
+    });
+
+    //우클릭 방지
+    main.addEventListener('contextmenu', function(event){
+        event.preventDefault();
+    });
+});
+
+//바로가기 생성 및 변경 함수
+function setLink(itemLinkN, num) {
+    //크롬 스토리지에 동기화된 사용자 바로가기 데이터 가져오기
+    chrome.storage.sync.get(`Link${num}`, function(data) {
+        //json에서 Ajax로 페이지 이미지와 링크 불러오기
+        let linkN = data[`Link${num}`];
+
+        fetch(chrome.extension.getURL("../data/page.json"))
+        .then((response) => response.json())
+        .then(function (jsonData) {
+            let URL = jsonData[`${linkN}`].URL; 
+            let IMG = jsonData[`${linkN}`].IMG;
+
+            //페이지 클릭 이벤트 추가 
+            clickOpenNewTeb(itemLinkN, URL);
+
+            //이미지 및 텍스트 생성
+            let linkImage = document.createElement('img');
+            let linkText = document.createElement('p');
+
+            linkImage.setAttribute('src', IMG);
+            linkText.textContent = linkN;
+
+            itemLinkN.appendChild(linkImage);
+            itemLinkN.appendChild(linkText);
+        });
+    });
 }
+
 
 //클릭 이벤트 및 바로가기 연결 함수
 function clickOpenNewTeb(element, URL){
@@ -147,4 +136,10 @@ function clickOpenNewTeb(element, URL){
     });
 
     element.style.cursor = "pointer";
+}
+
+//검색 함수
+function search(query){
+    let queryLink = "https://www.ajou.ac.kr/kr/search.do?qt=" + query;
+    window.open(queryLink);
 }
