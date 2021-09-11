3cc9eb58823d49c157471c56765e5c355565db23
Yeongbeom Song <gnyf0915@gmail.com>
2021-09-11 12:39:27 +0900

    Minor: Edit default setting
    바로가기 기본 설정 순서 변경

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({Color : "white", Department : "나의 학과", Link1: "홈페이지", Link2: "포탈", Link3: "학사서비스", Link4: "학사일정", Link5: "아주Bb", Link6: "전자출석부", Link7: "요람", Link8: "에브리타임", Link9: "공지사항", Link10: "중앙도서관", Link11: "아주허브", Link12: "나의 학과"}, function() {
+    chrome.storage.sync.set({Color : "white", Department : "나의 학과", Link1: "홈페이지", Link2: "포탈", Link3: "학사서비스", Link4: "공지사항", Link5: "아주Bb", Link6: "전자출석부", Link7: "요람", Link8: "에브리타임", Link9: "학사일정", Link10: "중앙도서관", Link11: "아주허브", Link12: "나의 학과"}, function() {
         console.log();
     });
 });
